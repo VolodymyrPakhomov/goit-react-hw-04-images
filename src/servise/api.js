0e4613d4ca556e10a65d340299cc8d0cf0e50023
VolodymyrPakhomov <vova.zest997@gmail.com>
2023-01-11 02:06:19 +0200

    1

--- src/servise/api.js
+++ src/servise/api.js
@@ -5,6 +5,7 @@ const API_KEY = '30790875-9fd5bd306a19ab25e45c54f4c';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 axios.defaults.params = { key: API_KEY, image_type: 'photo', orientation: 'horizontal', per_page: '12', };
 
+
 export const fetchImages = async (query, page) => {
   const params = {
     q: query,
